4c694472c9d015a0b74db253c9cd063a26a53435
Srihari Sriraman <srihari@nilenso.com>
2020-01-22 09:44:14 +0530

    Add confirmed sessions to talks data

--- src/cljs/inclojure_website/data.cljs
+++ src/cljs/inclojure_website/data.cljs
@@ -68,204 +68,58 @@ the Head of Technology for Quintype, a builder of products for digital media com
                       :schedule "https://inclojure.org/2016/#schedule"}]}})
 
 (defonce selected-talks
-  [{:duration "30m" :title "Registrations" :time "8:40 am"}
-   {:duration "10m" :title "Welcome" :time "9:20 am"}
+  [#_{:duration "30m" :title "Registrations" :time "8:40 am"}
+   #_{:duration "10m" :title "Welcome" :time "9:20 am"}
+   #_{:duration "60m" :title "BoF (Birds of Feather) sessions" :time "5:30 pm"}
+   #_{:duration "5m" :title "Ending note (10m)" :time "6:30 pm"}
+   #_{:duration "3h" :title "After party sponsored by Helpshift" :time "7:30 pm"}
 
    {:duration       "40m"
     :type           "Full talk"
-    :time           "9:30 am"
+    ;:time           "9:30 am"
     :selected-talk? true
-    :speakers       [{:name    "Eric Normand"
-                      :link    "https://twitter.com/ericnormand"
-                      :picture "images/speakers/eric-normand.jpg"
-                      :bio     "Eric is a writer, trainer, and speaker. He runs purelyfunctional.tv, and organises Clojure SYNC."}]
-    :company        "Lispcast"
-    :title          "You are in a maze of deeply nested maps, all alike"
-    :abstract       "People complain a lot about difficulties with
-    deeply nested maps in Clojure. I've never had that problem. I
-    looked at other people's code to see what they were doing to get
-    into trouble. In this talk you'll get a good idea of the wrong
-    turns people take and you'll leave with some techniques to find
-    your way out and to avoid getting lost in the first place."}
+    :speakers       [{:name    "Bozhidar Batsov\n"
+                      :link    "https://twitter.com/bbatsov"
+                      :picture "images/speakers/bozhidar-batsov.jpg"
+                      :bio     "Bozhidar is the maintainer of CIDER, nREPL, a dozen related projects, and the editor of the community Clojure style guide. Most people would probably describe him as an Emacs zealot (and they would be right). He's also quite fond of the Lisp family of languages, functional programming in general and Clojure in particular."}]
+    :company        "Toptal"
+    :title          "The Future of Clojure Tooling"
+    :abstract       "Ah, the future! Such an exciting topic! It’s always a lot of fun to speculate on what will follow next, isn’t it? You know what’s twice as exciting (at least for programmers) - discussing the future of Clojure development tools, those underappreciated workhorses that make our lives hacking with Clojure easier, more fun and more productive.\n\nIn this talk we’ll examine the current state of the broader Clojure tooling ecosystem, contrast the approaches taken by the most popular development tools out there, and try to give you a sense of their strengths and weaknesses. We’ll then skip to a discussions of some interesting efforts to improve the existing tool stack, happening right now in the community, and dream about the day they’ll come to fruition. And the day right after it.\n\nIdeally, when all is set and done you’ll have a much better understanding of how popular Clojure development tools are working, and more appreciation for all the efforts that go into them. Perhaps you’ll even get inspired enough to start contributing to some tools. :-)"}
 
    {:duration       "40m"
     :type           "Full talk"
-    :time           "10:20 am"
+    ;:time           "10:20 am"
     :selected-talk? true
-    :speakers       [{:name    "Abhinav Sarkar"
-                      :link    "http://twitter.com/abhin4v"
-                      :picture "images/speakers/abhinav-sarkar.png"
-                      :bio     "Abhinav works as a Principal Engineer
-                      at Flipkart. Abhinav has worked extensively in
-                      Clojure, Go and Java on large distributed
-                      systems for past ten years. He has been dabbling
-                      in functional programming for past few years and
-                      has been exploring Haskell, Erlang and ML. While
-                      not programming, you can find him riding his
-                      bicycle around the city."}]
-    :company        "Flipkart"
-    :title          "Introduction to Concurrency"
-    :abstract       "Clojure is widely known as a functional
-    programming language. But it has a great support for a wide
-    variety of concurrency models too. We’ll explore the various
-    concurrency models and primitives supported by Clojure including
-    threads, agents, STM and core.async goroutines and channels."}
-
-   {:duration "30m" :type "Tea Break" :time "11:00 am" }
-
-   {:duration       "10m"
-    :type           "Lightning talk"
-    :time           "11:30 am"
-    :selected-talk? true
-    :speakers       [{:name    "Kapil Reddy"
-                      :link    "https://twitter.com/kapilreddy"
-                      :picture "images/speakers/kapilreddy.jpg"
-                      :bio     "Kapil is a senior backend engineer at Helpshift"}]
-    :title          "Creating and evolving contracts with Clojure.spec"
-    :company        "Helpshift"}
-
-   {:duration       "20m"
-    :type           "Crisp talk"
-    :time           "11:45 am"
-    :selected-talk? true
-    :speakers       [{:name    "Rubal Jabbal"
-                      :link    "https://in.linkedin.com/in/rubal-jabbal-4ba8b99a"
-                      :picture "images/speakers/rubal-jabbal.jpeg"
-                      :bio     "Rubal loves to code, and loves sharing the knowledge! She is interested in photography, calligraphy and is an avid bathroom singer!"}
-                     {:name    "Pavitra Shetty"
-                      :link    "https://www.linkedin.com/in/pavitra-shetty-22877418"
-                      :picture "images/speakers/pavithra-shetty.jpeg"
-                      :bio     "Pavitra is a backend Clojure developer at Helpshift."}],
-    :company        "Helpshift"
-    :title          "Using components in production"
-    :abstract       "In this talk we introduce the problems faced in
-    our current architecture [read - giant monolith architecture] The
-    issues with multiple dependencies for a project and problems faced
-    when there is no clear boundary."}
+    :speakers       [{:name    "Pratik Karki"
+                      :link    "https://twitter.com/theawesomekarki"
+                      :picture "https://pbs.twimg.com/profile_images/885119064011493377/UAGBtWCG_400x400.jpg"
+                      :bio     "Hello, I’m Pratik Karki. I’m a programmer, a lifelong learner, Light Table maintainer, open-source contributor and GSoC‘18 student. I love to pop the hood of software and look inside, move a few things and close the hood,(80% of the time, I mess something up.) I’m a polyglot programmer(IOW: Jack of all trades). I love to talk about concurrency, optimizations, language design, editors and algorithms. I work as a Software Engineer at Leapfrog Technology Inc., Nepal. When I’m not programming, I read a lot, to be a better programmer and my favorite super-hero is Ken Thompson. BTW, I ❤ Clojure!\n\n"}]
+    :company        "Leapfrog Technology Inc., Nepal"
+    :title          "Light Table: The strange and wonderful saga of a pure Clojure editor."
+    :abstract       "On a beautiful spring day in 2012, Chris Granger showed a new concept of an IDE which was simple like an editor but, was full-fledged like IDE. This was way before VSCode and Atom, it showed what Clojure/Script can do with less code and was a preferred editor for people coming to Clojure/Script ecosystem for many years. Now, the time has changed and Chris left the project and so did, other core developers who shaped Light Table-the killer Clojure App. Now, years have gone by, without significant improvement in Light Table, so, yours truly volunteered to revive Light Table. Light Table introduced some awesome ideas of BOT architecture(Behavior, Objects, Tag). Getting Clojure book has also used its code for example in its “State” Chapter. The different ideas of adding plugins, docs embedded into the editor, eval feature inside the editor is cool and the code that powers it is in pure Clojure/script. And, this being written in the early days of ClojureScript i.e. before ClojureScript hit version 1.x.x, we can compare the ClojureScript as of today and can understand design changes it took from its early days. This talk will then, describe issues having tightly coupled a lot of functionalities with external libraries. Additionally, I would discuss using much better design to re-write Light Table, design inspired by a new-gen editor like Xi editor and the concepts behind toolings from clojure-emacs.\nLastly, I would like to give a demo and release a new Light Table based on new architectures mentioned earlier but, built on the foundation it stood on."}
 
    {:duration       "40m"
     :type           "Full talk"
-    :time           "12:10 pm"
+    ;:time           "10:20 am"
     :selected-talk? true
-    :speakers       [{:name    "Martin Klepsch"
-                      :link    "http://twitter.com/martinklepsch"
-                      :picture "images/speakers/martin-klepsch.jpg"
-                      :bio "Martin is a Clojure/Script consultant who
-                      never expected to work on a documentation
-                      platform. Before starting cljdoc he helped run
-                      various community events in Berlin, created
-                      CLJSJS and contributed to lots of Clojure
-                      OpenSource projects like Boot, the ClojureScript
-                      compiler, re-frame and more. He’s also
-                      organising a conference in Europe beginning of
-                      August next year, called Heart Of Clojure."}]
-    :title          "Cljdoc — Better documentation tooling for the Clojure/Script ecosystem"
-    :company        "cljdoc"
-    :abstract       "Documentation is a key part of working with open
-    source libraries. Good documentation makes programmers more
-    productive. It’s what makes simple things easy. In this talk we
-    will discuss cljdoc, a website that provides API docs and more
-    advanced documentation tooling for the Clojure community."}
-
-   {:duration       "20m"
-    :type           "Crisp talk"
-    :time           "1:00 pm"
-    :selected-talk? true
-    :speakers       [{:name    "Mourjo Sen"
-                      :link    "http://twitter.com/mourjo_sen"
-                      :picture "images/speakers/mourjo-sen.jpg"
-                      :bio     "Mourjo is a backend Clojure developer at Helpshift."}]
-    :company        "Helpshift"
-    :title          "Writing a fair task execution framework"
-    :abstract       "How to ensure that in a multi-tenant business on
-    the same infrastructure, one big customer will not bottleneck
-    every single maintenance task you have to perform on all
-    customers’ data? With the help of Clojure’s concurrency and
-    core.async, the answer to this question was intuitive and
-    effective."}
-
-   {:duration "1h 20m" :type "Lunch" :time "1:20 pm"}
-
-
-   {:duration       "10m"
-    :type           "Lightning talk"
-    :time           "2:40 pm"
-    :selected-talk? true
-    :speakers       [{:name    "Divyansh Prakash"
-                      :link    "https://twitter.com/therealdivs1210"
-                      :picture "images/speakers/divyansh.jpeg"
-                      :bio     "Divyansh is a software developer at nilenso"}]
-    :company        "nilenso"
-    :title          "async/await in Clojure using conquerant"}
-
-   {:duration       "20m"
-    :type           "Crisp talk"
-    :time           "2:50 pm"
-    :selected-talk? true
-    :speakers       [{:name    "Neha Mishra"
-                      :link    "https://in.linkedin.com/in/neha-mishra-563674ab"
-                      :picture "images/speakers/neha-mishra.jpg"
-                      :bio     "Neha is a backend Clojure developer at Helpshift Technologies."}]
-    :company        "Helpshift"
-    :title          "Chanakya: Building composable logical rules"
-    :abstract       "We will discuss our in-house lightweight rule
-    engine library. The talk will demonstrate a generic library for
-    building and executing a complex rule by composing simple logical
-    filters."}
+    :speakers       [{:name    "Mayur Jadhav"
+                      :link    "https://twitter.com/mayurJ13"
+                      :picture "https://pbs.twimg.com/profile_images/477076895716999169/leMTFwOa_400x400.jpeg"
+                      :bio     "Hi, I am co-founder of Dataorc, a Data oriented startup based out of Pune. I started my professional journey with Clojure at my previous company Helpshift and have been coding in same for last 6 years. Even at Dataorc, almost every project have some part developed in Clojure. I have used Clojure for architecting automation frameworks, building super scalable backends, distributed crawlers, munching TBs of data with it."}]
+    :company        "Dataorc"
+    :title          "Building data platforms from business stores using Clojur"
+    :abstract       "You have just bootstrapped and is catering thousands(or maybe millions) of happy users. Like most good applications, tech stack starts with a battle-tested RDBMS/NoSQL crud operations. Then, you start looking into user behavior, user’s interaction with the system to provide a customized experience or to deliver the next set of cool features. The ideal way to achieve such analysis is by sending custom events that can derive required metrics. Another hassle free approach is to capture change data of existing databases as an event stream. In this session, I will discuss the latter approach, it’s benefits and kind of use cases we solved with the power of Clojure. Clojure is a core part this architecture which handles spawning/destroying on-demand EMR clusters independent of cloud providers, DAG execution of EMR jobs, etc. Clojure REPL helped to speed up our development by reducing the time required to write and validate adhoc EMR queries."}
 
    {:duration       "40m"
     :type           "Full talk"
-    :time           "3:15 pm"
+    ;:time           "10:20 am"
     :selected-talk? true
-    :speakers       [{:name    "Shantanu Kumar"
-                      :link    "http://twitter.com/kumarshantanu"
-                      :picture "images/speakers/shantanu-kumar.jpg"
-                      :bio     "Shantanu is a Principal Engineer at
-                      Concur, building the core services platform
-                      using Clojure for fault tolerance and
-                      scale. Author of the book “Clojure High
-                      Performance Programming”, published by Packt
-                      publishing. Author of several Open Source
-                      Clojure libraries at
-                      https://github.com/kumarshantanu. Earlier worked
-                      at Runa (acquired by Staples) where he was using
-                      Clojure. Lives in Bangalore, India."}]
-    :company        "Concur"
-    :title          "Bract: A minimal DRY app framework"
-    :abstract       "Most Clojure applications have a lot in
-    common. While libraries do the heavy lifting for a specific
-    purpose, everything still needs to be glued together. I will talk
-    about an a-la-carte glue framework that brings uniformity and
-    structure to various types of applications."}
+    :speakers       [{:name    "Adam Helinski"
+                      :link    "https://twitter.com/dvlopt"
+                      :picture "https://pbs.twimg.com/profile_images/1217504173312421888/a85RMokD_400x400.jpg"
+                      :bio     "Former illusionist, a bit of a pianist, now a psychologist. Also a hammock philosopher and tamer of electrons. After studying neuroscience, Adam Helinski became a full-time parens whisperer in the kingdom of IoT where his slight sense of scientific madness lead him to the unique choice of using Clojure for prototyping connected devices. After those few years of exile away from psychology, he now expertiments with using Clojure in psychotherapy.\n\n"}]
+    :company        ""
+    :title          "Clojure of Things"
+    :abstract       "You have just bootstrapped and is catering thousands(or maybe millions) of happy users. Like most good applications, tech stack starts with a battle-tested RDBMS/NoSQL crud operations. Then, you start looking into user behavior, user’s interaction with the system to provide a customized experience or to deliver the next set of cool features. The ideal way to achieve such analysis is by sending custom events that can derive required metrics. Another hassle free approach is to capture change data of existing databases as an event stream. In this session, I will discuss the latter approach, it’s benefits and kind of use cases we solved with the power of Clojure. Clojure is a core part this architecture which handles spawning/destroying on-demand EMR clusters independent of cloud providers, DAG execution of EMR jobs, etc. Clojure REPL helped to speed up our development by reducing the time required to write and validate adhoc EMR queries."}
 
-   {:duration "40m" :type "Break" :time "4:00 pm"}
-
-   {:duration       "10m"
-    :type           "Lightning talk"
-    :time           "4:40 pm"
-    :selected-talk? true
-    :speakers       [{:name    "Aditya Chowdhry"
-                      :link    "https://twitter.com/adityachowdhry9"
-                      :picture "images/speakers/aditya.jpg"
-                      :bio     "Product engineer at Go-Jek"}]
-    :company        "Go-Jek"
-    :title          "Afraid of Clojure error stacktraces? Fear no more!"}
-
-   {:duration       "40m"
-    :type           "Full talk"
-    :time           "4:50 pm"
-    :speakers       [{:name    "Anirudh Vyas"
-                      :link    "http://twitter.com/Anirudh2403"
-                      :picture "images/speakers/anirudh-vyas.jpg"}]
-    :company        "Go-Jek"
-    :selected-talk? true
-    :title          "Evolution of a stream processing framework"
-    :abstract       "As we move to an event-driven architecture in
-    Gojek, Ziggurat, our stream processing framework written in
-    clojure acts as the central platform that enables developers to
-    rapidly iterate on and develop applications in a day that solve
-    problems ranging from processing payments to geofencing."}
-
-   {:duration "60m" :title "BoF (Birds of Feather) sessions" :time "5:30 pm"}
-   {:duration "5m" :title "Ending note (10m)" :time "6:30 pm"}
-   {:duration "3h" :title "After party sponsored by Helpshift" :time "7:30 pm"}])
+])
