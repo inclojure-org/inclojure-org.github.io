2a7ed5ffb96cc10691a9ecb3722735873c7bce1e
Ravindra Jaju <jaju@msync.org>
2023-12-27 22:40:10 +0530

    [Check] Added Shantanu's profile in the committee data - to validate how it appears, before adding others

--- src/cljs/inclojure_website/data.cljs
+++ src/cljs/inclojure_website/data.cljs
@@ -47,7 +47,15 @@
     :avatar "images/team/vkrm.png"}])
 
 (defonce talk-selectors
-  [])
+  [
+   {:name "Shantanu Kumar"
+    :avatar "images/committee/shantanu.png"
+    :twitter "https://twitter.com/kumarshantanu"
+    :github "kumarshantanu.github.io"
+    :www "https://www.linkedin.com/in/shantanuk06/"
+    :alt "Shantanu Kumar"
+    :bio "Shantanu is an experienced software artisan. He has interests in distributed systems, functional programming, performance and scalability.\nHe has been a user of Clojure for a long time professionally, and has published multiple open source libraries. He also has a book to his name on the topic of high performance programming in relation to Clojure."}
+   ])
 
 (defonce previous-editions
   {:bangalore {:map "images/bangalore-map-caption.png"
